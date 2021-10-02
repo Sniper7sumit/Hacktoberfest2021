44195331051a07446919eeb7f4d9a3858be1e692
MompatiPaso <91776054+MompatiPaso@users.noreply.github.com>
2021-10-02 09:15:54 +0200

    Add files via upload

---
+++ Check If A Number Is Odd Or Even.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main() {
+int x;
+cout << "Enter a number";
+cin >> x;
+if (x%2==1) {
+    cout << "number is odd";
+}
+else {
+   cout << "number is even";
+ }
+  return 0;
+}
