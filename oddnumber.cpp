a02fd6b8b97fa2352b0a0ded84e4f18c2dbe03c0
Hosh-Mohammad <92144356+Hosh-Mohammad@users.noreply.github.com>
2021-10-08 13:01:29 +0500

    created Odd number program

---
+++ oddnumber.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int i,num;
+    cout<<"Print all odd numbers until\n";
+    cin>>num;     
+    cout<<"Odd numbers from 1 to"<<num<< are\n";
+    for(i=1; i<=num; i++){
+        if(i%2==1)
+        {
+            cout<<i<<"\n";
+        }
+    }
+getch();
+    return 0;
+}
