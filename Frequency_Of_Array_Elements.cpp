094c24eb64a871385ba8280e7c83b237d5258a15
Sudhanshu Ranjan <77230416+SudhansuuRanjan@users.noreply.github.com>
2021-10-09 19:50:05 +0530

    Created Frequency_Of_Array_Elements.cpp
    Added program to print frequency table of array elements.

--- Frequency_Of_Array_Elements.cpp
+++ Frequency_Of_Array_Elements.cpp
@@ -1,10 +1,12 @@
-
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
     int n;
     cout << "Enter the size of array : "<<endl;
     cin>>n;
+  
+    //Array input 
+  
     cout << "\nEnter the array elements : " << endl;
     int arr[n];
     for (int i = 0; i < n; i++)
@@ -12,37 +14,44 @@ int main(){
        cin >> arr[i]; 
     }
 
+    //printing given array
+  
     cout << "\nYour Array : " << endl;
     for (int i = 0; i < n; i++)
     {
        cout << arr[i]<<"  "; 
     }
    
-    int fr[n];
+    int f[n];
     int visited = -11;    
-
+    
+    //counting the array elements and marking the visited elements
+  
     for(int i = 0; i < n; i++){    
         int count = 1;    
         for(int j = i+1; j < n ; j++){    
              if(arr[i] == arr[j]){    
                 count++;    
-                fr[j] = visited;    
+                f[j] = visited;    
               }    
               }    
 
-        if(fr[i] != visited)    
-           fr[i] = count;    
+        if(f[i] != visited)    
+           f[i] = count;    
 
     }    
-
+  
+  
+    // Printing the frequency table
+  
     printf("\n\n\nFrequency Table :\n\n--------------------------\n");    
     printf(" Element    |    Frequency\n");    
     printf("--------------------------\n");    
     for(int i = 0; i < n ; i++){    
-        if(fr[i] != visited){    
+        if(f[i] != visited){    
             printf("     %d", arr[i]);    
             printf("      |    ");    
-            printf("  %d\n", fr[i]);    
+            printf("  %d\n", f[i]);    
         }    
     }    
     printf("--------------------------\n");
