b046cb0b782cabba78b6ee526c83489bd993be3d
33hm <91753013+33hm@users.noreply.github.com>
2021-10-13 09:28:54 +0530

    Add files via upload

---
+++ A_AtCoder_Quiz_2.cpp
@@ -0,0 +1,45 @@
+#pragma GCC optimize ("trapv")
+#include <bits/stdc++.h>
+#include<algorithm>
+#include <vector>
+#include<cstring>
+#include<cstdlib>
+#include<cmath>
+#include<cstdlib>
+#include<sstream>
+#include <string.h>
+using namespace std;
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define  rep(i,n,v) for(i=n;i<v;i++)
+#define per(i,n,v) for(i=n;i>v;i--)
+#define ff first 
+#define ss second 
+#define pp pair<ll,ll>
+#define ll  long long
+#define ld  long double
+#define endl "\n"
+ 
+const ll mod=1e9+7;
+void solve()
+{
+ll  n, a=0,b=0,m=1, c=-1,k=0, i=0, j=0, l=1e9+5;
+string s,p, q;
+cin>>n;
+if(n<40 && n>=0) cout<<40-n;
+else if(n<70 && n>=40) cout<<70-n;
+else if(n<90 && n>=70) cout<<90-n;
+else cout<<"expert";
+}
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin. tie(0);cout. tie(0);
+ll t=1;
+//cin>>t;
+while(t--)
+{
+solve();
+}
+return 0;
+}
