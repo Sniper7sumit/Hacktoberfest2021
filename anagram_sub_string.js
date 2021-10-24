8f8f0a2f8721b8a76b081aab6987e4cbb694ac40
donalawa <54797997+donalawa@users.noreply.github.com>
2021-10-24 01:48:34 -0700

    Anagram Substring

---
+++ anagram_sub_string.js
@@ -0,0 +1,15 @@
+
+let w = 'abx';
+let S = 'abxaba';
+
+let holdWords = S.split("");
+
+
+for(let i = 0; i < holdWords.length; i++) {
+    let currWord = holdWords.slice(i,i + w.length).join("");
+
+    if(currWord.split("").sort().join("") == w.split("").sort().join("")) {
+        console.log("Current Word Is Anagram Of w")
+        console.log(currWord);
+    }
+} 
