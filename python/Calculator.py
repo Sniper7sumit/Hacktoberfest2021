c0f75cfbcfa6002be3a028e28b2a652c131b739c
Akshat Verma <112319520+Akshat162001@users.noreply.github.com>
2023-07-21 17:54:45 +0530

    added another python code

---
+++ python/Calculator.py
@@ -0,0 +1,47 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Cannot divide by zero."
+
+def calculator():
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("Enter 'q' to quit")
+
+    while True:
+        choice = input("Enter choice (1/2/3/4/q): ")
+
+        if choice in ['1', '2', '3', '4']:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+
+            if choice == '1':
+                print("Result:", add(num1, num2))
+            elif choice == '2':
+                print("Result:", subtract(num1, num2))
+            elif choice == '3':
+                print("Result:", multiply(num1, num2))
+            elif choice == '4':
+                print("Result:", divide(num1, num2))
+        elif choice.lower() == 'q':
+            print("Calculator exiting.")
+            break
+        else:
+            print("Invalid input. Please try again.")
+
+if __name__ == "__main__":
+    print("Simple Calculator")
+    calculator()
